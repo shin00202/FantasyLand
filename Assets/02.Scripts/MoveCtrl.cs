5d6ceeafe0f261253c53a6989d6d2c051a1f3175
shin00202 <66237347+shin00202@users.noreply.github.com>
2020-10-23 13:31:07 +0900

    30108.10.23(신승용)

---
+++ Assets/02.Scripts/MoveCtrl.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCtrl : MonoBehaviour
+{
+
+    public enum MoveType{
+        WAY_POINT,
+        LOOK_AT,
+        GEAR_VR
+    }
+
+    public MoveType moveType = MoveType.WAY_POINT;
+    public float speed = 1.0f;
+    public float damping = 3.0f;
+
+    // Start is called before the first frame update
+    public Transform[] points;
+    private Transform tr;
+    private CharacterController cc;
+    private Transform camTr;
+    private int nextIdx = 1;
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        cc = GetComponent<CharacterController>();
+        camTr = Camera.main.GetComponent<Transform>();
+        GameObject wayPointGroup = GameObject.Find("WayPointGroup");
+        if (wayPointGroup != null)
+        {
+            points = wayPointGroup.GetComponentsInChildren<Transform>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (moveType) 
+        {
+            case MoveType.WAY_POINT:
+                MoveWayPoint();
+                break;
+            case MoveType.LOOK_AT:
+                MoveLookAt(1);
+                break;
+            case MoveType.GEAR_VR:
+                break;
+        }
+        
+    }
+
+    void MoveWayPoint()
+    {
+        Vector3 direction = poin
+    }
+}
